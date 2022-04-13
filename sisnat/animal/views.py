7c2f47b9ff5ef1b66784359efd5117ecb9403075
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2022-04-13 17:20:37 -0300

    Create home and pdf views

--- sisnat/animal/views.py
+++ sisnat/animal/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def home_page(request):
+    return render(request, 'sisnat/home_page.html')
+
+@login_required(login_url='/sisnat/')
+def pdf_page(request):
+    return render(request, 'sisnat/pdf_page.html')
