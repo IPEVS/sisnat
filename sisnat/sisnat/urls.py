ad153762b5d35484c2b784d38d2900b6b4c95ce3
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2022-04-13 17:21:18 -0300

    Update admin url and add new urls

--- sisnat/sisnat/urls.py
+++ sisnat/sisnat/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from animal import views
+
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
+from django.conf import settings 
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/home_page/')),
+    path('sisnat/', admin.site.urls),
+    path('home_page/', views.home_page),
+    path('pdf/', views.pdf_page),
 ]
+if settings.DEBUG:
+    urlpatterns += static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
