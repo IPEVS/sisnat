f6a4b5bc860914ec30c1144d5a114bf001894c76
Luis Fernando Benatto <49990149+LuisFernandoBenatto@users.noreply.github.com>
2022-04-16 13:06:19 -0300

    Improve animals report

--- sisnat/animal/views.py
+++ sisnat/animal/views.py
@@ -3,8 +3,8 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'sisnat/home_page.html')
+    return render(request, 'animal/home_page.html')
 
 @login_required(login_url='/sisnat/')
 def pdf_page(request):
-    return render(request, 'sisnat/pdf_page.html')
+    return render(request, 'animal/pdf_page.html')
